f83a9b3c1ff8c99271afd5254658029c614f1e35
Thakur Ankush Singh <thakurankushsingh1902@gmail.com>
2024-07-17 14:07:59 +0530

    Leader Hero Birthday

---
+++ components/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className="h-screen w-screen flex justify-center items-center bg-[#c5DDf1]">
+            <h1 className="text-4xl font-bold text-[#333]">Welcome to the home page!</h1>
+        </div>
+    )
+}
+
+export default Hero
